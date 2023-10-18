d0c8f70df14aa0ed55b13bf4810aab7a20b39757
Tyoukai <zhang_k_w@163.com>
2023-10-18 17:02:18 +0800

    增加了tushare图形展示

--- basic-stock/tushare_test.py
+++ basic-stock/tushare_test.py
@@ -1,10 +1,23 @@
 import tushare as ts
+import matplotlib.pyplot as plt
+import numpy as np
 
 ts.set_token('99c90b0a4e59eb836aebcfd89e6d0aa62b77212fabaa9c7a5c81e888')
 pro = ts.pro_api()
 
-df = pro.daily(ts_code='000001.SZ', start_date='20230901', end_date='20230930')
-print(df)
+df = pro.daily(ts_code='000001.SZ', start_date='20230901', end_date='20230907')
+# print(df)
+
+fig = plt.figure(1, (8, 6))
+ax = fig.add_subplot(111)
+
+ax.set_ylim([7, 13])
+ax.set_yticks(np.linspace(7, 13, 14))
+
+ax.plot(df['trade_date'], df['close'], 'r-^', label='close')
+ax.legend(loc=3, labelspacing=2, handlelength=3, fontsize=14, shadow=True)
+
+plt.show()
 
 
 
