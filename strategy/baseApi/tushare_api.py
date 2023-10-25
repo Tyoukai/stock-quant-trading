fa07a17fdf96a81b8367f1d1760822aa070b41f9
Tyoukai <zhang_k_w@163.com>
2023-10-25 20:35:28 +0800

    增加了10日均线

--- strategy/baseApi/tushare_api.py
+++ strategy/baseApi/tushare_api.py
@@ -5,4 +5,7 @@ def get_daily_stock(code, start_date, end_date):
     ts.set_token('99c90b0a4e59eb836aebcfd89e6d0aa62b77212fabaa9c7a5c81e888')
     pro = ts.pro_api()
     df = pro.daily(ts_code=code, start_date=start_date, end_date=end_date)
+    df = df.reindex(df.index[::-1])
     return df
+
+
--- strategy/strategy002/sma_single_moving_average.py
+++ strategy/strategy002/sma_single_moving_average.py
@@ -1,6 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ..baseApi import tushare_api
+import pandas as pd
+import tushare as ts
 
-df = tushare_api.get_daily_stock('000001.SZ', '20230901', '20230930')
+ts.set_token('99c90b0a4e59eb836aebcfd89e6d0aa62b77212fabaa9c7a5c81e888')
+pro = ts.pro_api()
+df = pro.daily(ts_code='000001.SZ', start_date='20230818', end_date='20230928')
+df = df.reindex(df.index[::-1])
+
+calculate_df = df.loc[19:0, ['trade_date', 'close']]
+avg_10 = []
+avg_value = []
+period = 10
+
+for close in df['close']:
+    avg_10.append(close)
+    if len(avg_10) > period:
+        del avg_10[0]
+    if len(avg_10) == period:
+        avg_value.append(np.mean(avg_10))
+del avg_value[len(avg_value) - 1]
+calculate_df['avg_10'] = avg_value
+# calculate_df = calculate_df.assign(avg_10=pd.Series(avg_value, index=calculate_df.index))
+print(calculate_df)
+
+fig = plt.figure(1, (15, 10))
+ax = fig.add_subplot(111)
+
+ax.plot(calculate_df['trade_date'], calculate_df['close'], 'b-', 'close')
+ax.plot(calculate_df['trade_date'], calculate_df['close'], 'r--', 'avg_10')
+
+ax.set_ylim([8, 12])
+ax.set_xticks(['20230901', '20230908', '20230915', '20230922', '20230928'])
+ax.legend(loc=3)
+plt.show()
+
+
+
+
 
