0ef93e7b7ab6299b00910bb02c244aa017f527fa
Tyoukai <zhang_k_w@163.com>
2023-11-01 21:54:26 +0800

    完成双平滑曲线回测

--- strategy/strategy002/dma_double_moving_average.py
+++ strategy/strategy002/dma_double_moving_average.py
@@ -18,26 +18,44 @@ calculate_df['order'] = calculate_df['single'].diff()
 calculate_df.fillna(0.0)
 print(calculate_df)
 
-fig = plt.figure(1, (15, 10))
-ax = fig.add_subplot(111)
-ax.plot(calculate_df['trade_date'], calculate_df['close'], 'b-', label='close')
-ax.plot(calculate_df['trade_date'], calculate_df['avg_5'], 'y--', label='avg_5')
-ax.plot(calculate_df['trade_date'], calculate_df['avg_10'], 'r--', label='avg_10')
-df_buy = calculate_df[['trade_date', 'close']][calculate_df['order'] > 0]
-df_sale = calculate_df[['trade_date', 'close']][calculate_df['order'] < 0]
-ax.scatter(df_buy['trade_date'], df_buy['close'], s=200, color='r', marker='^', label='buy')
-ax.scatter(df_sale['trade_date'], df_sale['close'], s=200, color='g', marker='v', label='sale')
-ax.legend(loc=3)
-ax.set_xticks(['20230901', '20230908', '20230915', '20230922', '20230928'])
-fig.show()
+# fig = plt.figure(1, (15, 10))
+# ax = fig.add_subplot(111)
+# ax.plot(calculate_df['trade_date'], calculate_df['close'], 'b-', label='close')
+# ax.plot(calculate_df['trade_date'], calculate_df['avg_5'], 'y--', label='avg_5')
+# ax.plot(calculate_df['trade_date'], calculate_df['avg_10'], 'r--', label='avg_10')
+# df_buy = calculate_df[['trade_date', 'close']][calculate_df['order'] > 0]
+# df_sale = calculate_df[['trade_date', 'close']][calculate_df['order'] < 0]
+# ax.scatter(df_buy['trade_date'], df_buy['close'], s=200, color='r', marker='^', label='buy')
+# ax.scatter(df_sale['trade_date'], df_sale['close'], s=200, color='g', marker='v', label='sale')
+# ax.legend(loc=3)
+# ax.set_xticks(['20230901', '20230908', '20230915', '20230922', '20230928'])
+# fig.show()
 
 
 # 回测
-init_cash = 20000
-positions = pd.DataFrame(index=calculate_df.index).fillna(0.0)
-positions['stock'] = calculate_df['signal'] * 100
-positions['stock value'] = positions['stock'].multiply(calculate_df['close'], axis=0)
+calculate_df['operation single'] = calculate_df['single'].diff()
+calculate_df = calculate_df.fillna(0)
+calculate_df['stock num'] = calculate_df['operation single'].cumsum() * 1500
+calculate_df['stock value'] = calculate_df['stock num'].multiply(calculate_df['close'], axis=0)
+yesterday_cash = 20000
+calculate_df['cash in hand'] = np.zeros((1, len(calculate_df)))[0]
 
+calculate_df['operation total value'] = calculate_df['operation single'].multiply(1500, axis=0).multiply(calculate_df['close'], axis=0)
+for index in calculate_df.index:
+    calculate_df.loc[index, 'cash in hand'] = yesterday_cash - calculate_df.loc[index, 'operation total value']
+    yesterday_cash = calculate_df.loc[index, 'cash in hand']
+calculate_df['total value'] = calculate_df['cash in hand'].add(calculate_df['stock value'], axis=0)
+print(calculate_df)
+
+fig = plt.figure(1, (10, 8))
+ax = fig.add_subplot(111)
+
+ax.plot(calculate_df['trade_date'], calculate_df['total value'], 'r-', label='total value')
+ax.plot(calculate_df['trade_date'], calculate_df['stock value'], 'b--', label='stock value')
+ax.legend(loc=3)
+ax.set_xticks(['20230901', '20230908', '20230915', '20230922', '20230928'])
+plt.grid(color='g', linestyle=':')
+plt.show()
 
 
 
