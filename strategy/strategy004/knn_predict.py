818b9695e6743d7f3b2b8a0dc7b59781b948e6cd
Tyoukai <zhang_k_w@163.com>
2023-11-08 22:18:19 +0800

    增加了knn的回测

--- strategy/strategy004/knn_predict.py
+++ strategy/strategy004/knn_predict.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
@@ -20,13 +19,13 @@ X = calculate_df[['open_close', 'high_low']]
 y = calculate_df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-knn_stock = KNeighborsClassifier(n_neighbors=95)
+knn_stock = KNeighborsClassifier(n_neighbors=10)
 knn_stock.fit(X_train, y_train)
 print(knn_stock.score(X_train, y_train))
 print(knn_stock.score(X_test, y_test))
 
 new_df = calculate_df.iloc[-20:]
-new_df['knn_predict_signal'] = knn_stock.predict(calculate_df[['open_close', 'high_low']])
+new_df['knn_predict_signal'] = knn_stock.predict(new_df[['open_close', 'high_low']])
 new_df['signal'] = new_df['knn_predict_signal'].diff()
 new_df = new_df.fillna(0)
 new_df['stock_num'] = new_df['signal'].cumsum() * 1500
@@ -41,10 +40,21 @@ for index in new_df.index:
 new_df['total'] = new_df['cash_in_hand'] + new_df['stock_value']
 new_df['knn_yield'] = ((new_df['total'] - init_cash) / init_cash) * 100
 
-# new_df['diff_close'] = new_df['close'] - new_df['close'].shift(1)
-# new_df = new_df.fillna(0)
-# new_df['001_signal'] = np.where(new_df['diff_close'] > 0, 1, 0)
+fig = plt.figure(1, (10, 8))
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
 
+ax1.plot(new_df['trade_date'], new_df['knn_yield'], 'r--', label='yield')
+ax1.plot(new_df['trade_date'], np.zeros((1, 20))[0], 'g--')
+ax1.set_xticks(['20230901', '20230908', '20230915', '20230922', '20230928'])
+ax1.legend(loc=3)
+
+ax2.plot(new_df['trade_date'], new_df['total'], 'r--', label='total')
+ax2.plot(new_df['trade_date'], np.ones((1, 20))[0] * 20000, 'g--')
+ax2.set_xticks(['20230901', '20230908', '20230915', '20230922', '20230928'])
+ax2.legend(loc=3)
+
+plt.show()
 
 
 
