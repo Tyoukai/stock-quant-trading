7d06dc7a813fc655e7888fdc94b816c9e00e44d7
Tyoukai <zhang_k_w@163.com>
2024-02-20 22:55:33 +0800

    完成启明星

--- strategy/candle-strategy/strategy017.py
+++ strategy/candle-strategy/strategy017.py
@@ -10,15 +10,50 @@ def hit_feature(code, start_date, end_date):
         if np.any(stock.isnull()):
             return False
         time.sleep(0.005)
-        x = np.arange(len(stock.index)).reshape(-1, 1)
-        stock['mid_close'] = (stock['open'] + stock['close']) / 2.0
-        lr = LinearRegression().fit(x, stock['mid_close'])
-        if lr.coef_ >= 0:
+
+        today_open = stock['open'].iloc[len(stock.index) - 1]
+        today_close = stock['close'].iloc[len(stock.index) - 1]
+        yesterday_high = stock['high'].iloc[len(stock.index) - 2]
+        yesterday_low = stock['low'].iloc[len(stock.index) - 2]
+        yesterday_open = stock['open'].iloc[len(stock.index) - 2]
+        yesterday_close = stock['close'].iloc[len(stock.index) - 2]
+        the_day_before_yesterday_open = stock['open'].iloc[len(stock.index) - 3]
+        the_day_before_yesterday_close = stock['close'].iloc[len(stock.index) - 3]
+
+        if the_day_before_yesterday_open <= the_day_before_yesterday_close:
+            return False
+        if today_close <= today_open:
             return False
 
+        if yesterday_open < yesterday_close and yesterday_close > the_day_before_yesterday_close:
+            return False
+        if yesterday_open > yesterday_close and yesterday_open > the_day_before_yesterday_close:
+            return False
 
+        if is_star(yesterday_open, yesterday_close, yesterday_high, yesterday_low):
+            x = np.arange(len(stock.index)).reshape(-1, 1)
+            stock['mid_close'] = (stock['open'] + stock['close']) / 2.0
+            lr = LinearRegression().fit(x, stock['mid_close'])
+            return lr.coef_ >= -0.2
+        return False
     except Exception as e:
         return False
+
+
+def is_star(open, close, high, low):
+    """
+    十字星定义， 实体长度小于总长度的10%
+    :param open:
+    :param close:
+    :param high:
+    :param low:
+    :return:
+    """
+    entity = abs(open - close)
+    total = abs(high - low)
+    if total == 0:
+        return False
+    return entity / total <= 0.1
 
 
 if __name__ == '__main__':
@@ -28,6 +63,6 @@ if __name__ == '__main__':
     """
 
     stock_df = list_stock_code_and_price_by_ak(None)
-    stock_df['star_signal'] = stock_df.apply(lambda x: hit_feature(x['code']), axis=1)
+    stock_df['star_signal'] = stock_df.apply(lambda x: hit_feature(x['code'], '', ''), axis=1)
     stock_df = stock_df[stock_df['star_signal']]
     print(stock_df)
