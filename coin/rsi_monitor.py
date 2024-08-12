5c1995e645934afe3cfd0dbd870493fd5f49ea4c
Tyoukai <zhang_k_w@163.com>
2024-08-12 21:53:59 +0800

    update

--- coin/rsi_monitor.py
+++ coin/rsi_monitor.py
@@ -90,7 +90,7 @@ def get_latest_k_line(symbol_local, interval_local, max_delta, end_time):
     :param end_time:
     :return:
     """
-    end_time = 1723214593000
+    end_time = 1723469353000
     # 计算开始时间
     if interval_local == '15m':
         unit = 15 * 60 * 1000
@@ -104,30 +104,33 @@ def get_latest_k_line(symbol_local, interval_local, max_delta, end_time):
         fifteen_minute_df['start_time'] = np.zeros(max_delta + 1)
         fifteen_minute_df['start_time'] = fifteen_minute_df['start_time'].astype(int)
         fifteen_minute_df['close'] = np.zeros(max_delta + 1)
-        fifteen_minute_df['close'] = fifteen_minute_df['close'].astype(str)
         fifteen_minute_df['low'] = np.zeros(max_delta + 1)
-        fifteen_minute_df['low'] = fifteen_minute_df['low'].astype(str)
         for i in range(max_delta + 1):
             tmp_one_minute_df = pd.DataFrame()
             if i == max_delta:
                 tmp_one_minute_df = one_minute_df.iloc[i * 15: len(one_minute_df.index)]
             else:
                 tmp_one_minute_df = one_minute_df[i * 15: i * 15 + 15]
-            min_low = tmp_one_minute_df['low'].min()
-            close = tmp_one_minute_df['close'].iloc[len(tmp_one_minute_df.index) - 1]
+            tmp_one_minute_df_low = (tmp_one_minute_df[['low']].copy()).astype('float')
+            tmp_one_minute_df_close = (tmp_one_minute_df[['close']].copy()).astype('float')
+            min_low = tmp_one_minute_df_low['low'].min()
+            close = tmp_one_minute_df_close['close'].iloc[len(tmp_one_minute_df.index) - 1]
             start_time = tmp_one_minute_df['start_time'].iloc[0]
             fifteen_minute_df.loc[i, 'start_time'] = start_time
             fifteen_minute_df.loc[i, 'close'] = close
             fifteen_minute_df.loc[i, 'low'] = min_low
+        fifteen_minute_df['close'] = fifteen_minute_df['close'].astype(str)
+        fifteen_minute_df['low'] = fifteen_minute_df['low'].astype(str)
         return fifteen_minute_df
     return pd.DataFrame()
 
 
 if __name__ == '__main__':
     client = Spot(base_url='https://api4.binance.com')
-    symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'XRPUSDT', 'TONUSDT', 'DOGEUSDT', 'ADAUSDT', 'TRXUSDT',
+    symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'XRPUSDT', 'TONUSDT', 'DOGEUSDT', 'ADAUSDT',
                'WBTCUSDT', 'AVAXUSDT', 'SHIBUSDT', 'DOTUSDT', 'BCHUSDT', 'LINKUSDT', 'LTCUSDT', 'NEARUSDT', 'MATICUSDT',
                'UNIUSDT', 'PEPEUSDT', 'ICPUSDT']
+
     for symbol in symbols:
         interval = '15m'
         rsi_calculate_cycle = 6
