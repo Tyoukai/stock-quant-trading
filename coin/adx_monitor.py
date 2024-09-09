ad33d1317833bb193c8eaba3049bc9e9ec2062c3
Tyoukai <zhang_k_w@163.com>
2024-09-09 22:41:03 +0800

    adx完成

--- coin/adx_monitor.py
+++ coin/adx_monitor.py
@@ -13,14 +13,15 @@ def calculate_dm(df_local):
     :return:
     """
     df_local['up_move'] = df_local['high'] - df_local['high'].shift(1)
-    df_local['down_move'] = df_local['low'].shift(-1) - df_local['low']
+    df_local['down_move'] = df_local['low'].shift(1) - df_local['low']
     df_local['dm_plus'] = np.zeros(len(df_local.index))
     df_local['dm_minus'] = np.zeros(len(df_local.index))
+    df_local = df_local.dropna(axis=0, how='any').reset_index(drop=True)
     for i in range(len(df_local.index)):
         if df_local.iloc[i]['up_move'] > max(df_local.iloc[i]['down_move'], 0):
-            df_local.loc[i]['dm_plus'] = df_local.iloc[i]['up_move']
+            df_local.loc[i, 'dm_plus'] = df_local.iloc[i]['up_move']
         if df_local.iloc[i]['down_move'] > max(df_local.iloc[i]['up_move'], 0):
-            df_local.loc[i]['dm_minus'] = df_local.iloc[i]['down_move']
+            df_local.loc[i, 'dm_minus'] = df_local.iloc[i]['down_move']
     return df_local
 
 
@@ -37,14 +38,14 @@ def calculate_adx(df_local, n):
     df_local['di_plus_n'] = np.zeros(len(df_local.index))
     df_local['di_minus_n'] = np.zeros(len(df_local.index))
 
-    df_local.loc[n - 1]['tr_n'] = np.mean(df_local['tr'].iloc[0:n])
-    df_local.loc[n - 1]['dm_plus_n'] = np.mean(df_local['dm_plus'].iloc[0:n])
-    df_local.loc[n - 1]['dm_minus_n'] = np.mean(df_local['dm_minus'].iloc[0:n])
+    df_local.loc[n - 1, 'tr_n'] = np.mean(df_local['TR'].iloc[0:n])
+    df_local.loc[n - 1, 'dm_plus_n'] = np.mean(df_local['dm_plus'].iloc[0:n])
+    df_local.loc[n - 1, 'dm_minus_n'] = np.mean(df_local['dm_minus'].iloc[0:n])
 
     for i in range(n, len(df_local.index)):
-        df_local.loc[i]['tr_n'] = df_local.loc[i - 1]['tr_n'] * (n - 1) / n + df_local.loc[i]['tr']
-        df_local.loc[i]['dm_plus_n'] = df_local.loc[i - 1]['dm_plus_n'] * (n - 1) / n + df_local.loc[i]['dm_plus']
-        df_local.loc[i]['dm_minus_n'] = df_local.loc[i - 1]['dm_minus_n'] * (n - 1) / n + df_local.loc[i]['dm_minus']
+        df_local.loc[i, 'tr_n'] = df_local.iloc[i - 1]['tr_n'] * (n - 1) / n + df_local.iloc[i]['TR']
+        df_local.loc[i, 'dm_plus_n'] = df_local.iloc[i - 1]['dm_plus_n'] * (n - 1) / n + df_local.iloc[i]['dm_plus']
+        df_local.loc[i, 'dm_minus_n'] = df_local.iloc[i - 1]['dm_minus_n'] * (n - 1) / n + df_local.iloc[i]['dm_minus']
     df_local['di_plus_n'] = 100 * df_local['dm_plus_n'] / df_local['tr_n']
     df_local['di_minus_n'] = 100 * df_local['dm_minus_n'] / df_local['tr_n']
     df_local['dx'] = abs(100 * (df_local['di_plus_n'] - df_local['di_minus_n']) /
@@ -52,42 +53,46 @@ def calculate_adx(df_local, n):
     df_local = df_local.dropna(axis=0, how='any').reset_index(drop=True)
 
     df_local['adx'] = np.zeros(len(df_local.index))
-    df_local.loc[n - 1]['adx'] = np.mean(df_local['adx'].iloc[0:n])
+    df_local.loc[n - 1, 'adx'] = np.mean(df_local['adx'].iloc[0:n])
     for i in range(n, len(df_local.index)):
-        df_local.loc[i]['adx'] = df_local.loc[i - 1]['adx'] + df_local.loc[i]['dx']
+        df_local.loc[i, 'adx'] = (df_local.iloc[i - 1]['adx'] * (n - 1) + df_local.loc[i]['dx']) / n
     return df_local
 
 
 def draw(df_local, symbol_local):
     fig = plt.figure(1, figsize=(6, 4))
     ax = fig.add_subplot(111)
-    ax.plot(df_local['start_time'], df_local['close'], 'r-,', label='close')
-    ax.plot(df_local['start_time'], df_local['di_plus_n'], 'b-,', label='ema')
-    ax.plot(df_local['start_time'], df_local['di_minus_n'], 'g-,', label='min')
-    ax.plot(df_local['start_time'], df_local['adx'], 'k-,', label='max')
+    # ax.plot(df_local['start_time'], df_local['close'], 'r-,', label='close')
+    ax.plot(df_local['start_time'], df_local['di_plus_n'], 'b-,', label='di_plus_n')
+    ax.plot(df_local['start_time'], df_local['di_minus_n'], 'g-,', label='di_minus_n')
+    ax.plot(df_local['start_time'], df_local['adx'], 'k-,', label='adx')
     ax.legend(loc=3)
     plt.title(label=symbol_local)
     plt.show()
 
 
 if __name__ == '__main__':
-    symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'XRPUSDT', 'TONUSDT', 'DOGEUSDT', 'ADAUSDT',
-               'WBTCUSDT', 'AVAXUSDT', 'SHIBUSDT', 'DOTUSDT', 'BCHUSDT', 'LINKUSDT', 'LTCUSDT', 'NEARUSDT', 'MATICUSDT',
-               'UNIUSDT', 'PEPEUSDT', 'ICPUSDT', 'APTUSDT', 'WBETHUSDT', 'ETCUSDT', 'SUIUSDT', 'STXUSDT',
-               'FETUSDT', 'FILUSDT']
+    # symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'XRPUSDT', 'TONUSDT', 'DOGEUSDT', 'ADAUSDT',
+    #            'WBTCUSDT', 'AVAXUSDT', 'SHIBUSDT', 'DOTUSDT', 'BCHUSDT', 'LINKUSDT', 'LTCUSDT', 'NEARUSDT', 'MATICUSDT',
+    #            'UNIUSDT', 'PEPEUSDT', 'ICPUSDT', 'APTUSDT', 'WBETHUSDT', 'ETCUSDT', 'SUIUSDT', 'STXUSDT',
+    #            'FETUSDT', 'FILUSDT']
+    symbols = ['BTCUSDT']
     # 循环周期
     cycle = 13
     for symbol in symbols:
         # 1、获取指定分钟k线图
-        result, fifteen_minute_df = get_latest_k_line(symbol, '15m', 67,
+        result, fifteen_minute_df = get_latest_k_line(symbol, '15m', 120,
                                                       int(datetime.datetime.now().timestamp() * 1000))
         if not result:
             continue
+        fifteen_minute_df['close'] = fifteen_minute_df['close'].astype(float)
+        fifteen_minute_df['low'] = fifteen_minute_df['low'].astype(float)
+        fifteen_minute_df['high'] = fifteen_minute_df['high'].astype(float)
         # 2、计算TR
         fifteen_minute_df = calculate_TR(fifteen_minute_df)
         # 3、计算DM+ DM-
         fifteen_minute_df = calculate_dm(fifteen_minute_df)
         # 4、计算DI+ DI- DX ADX
-        calculate_adx(fifteen_minute_df, cycle)
+        fifteen_minute_df = calculate_adx(fifteen_minute_df, cycle)
         # 7、图形展示
-        draw(fifteen_minute_df)
+        draw(fifteen_minute_df, symbol)
