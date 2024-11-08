ee5fffcd3f913b412354f4e4d1ebe03a64663af4
Tyoukai <zhang_k_w@163.com>
2024-11-08 18:02:04 +0800

    update

--- python-learning/cufflinks/cufflinks_normal_demo.py
+++ python-learning/cufflinks/cufflinks_normal_demo.py
@@ -1,9 +1,40 @@
 import cufflinks as cf
+import pandas as pd
 import plotly
 import numpy as np
 
-df = cf.datagen.ohlc()
-df['volume'] = np.ones(len(df.index))
+print(cf.__version__)
 setattr(plotly.offline, "__PLOTLY_OFFLINE_INITIALIZED", True)
-df.iplot(kind='candle', title='bar-chart', subplots=True, shape=[3, 1])
+
+# 柱状图
+# df = pd.DataFrame({'Category': ['A', 'B', 'C', 'D'], 'Value': [21, 32, 33, 20]})
+# df.iplot(kind='bar', x='Category', y='Value', xTitle='种类', yTitle='数量', title='直方图')
+
+# 新直方图 默认按照行维度进行分组
+# df = pd.DataFrame(np.random.randn(100, 4), columns='A B C D'.split())
+# local_df = df.head(10)
+# print(local_df)
+# local_df.iplot('bar')
+
+# 折线图 横坐标默认是df的index
+# df = pd.DataFrame(np.random.randn(100, 4), columns='A B C D'.split())
+# df.iplot()
+
+# 散点图
+# df = pd.DataFrame(np.random.randn(100, 4), columns='A B C D'.split())
+# df.iplot(kind='scatter', x='A', y='B', mode='markers', size=20)
+
+# 直方图
+# mm = np.random.randn(1000)
+# print(mm)
+# df = pd.DataFrame({'a': mm}, columns=['a'])
+# df.iplot(kind='histogram')
+
+# 同种类型子图
+# df = cf.datagen.histogram(4)
+# df.iplot(kind='histogram', subplots=True)
+
+# 不同种类型子图
+
+
 
