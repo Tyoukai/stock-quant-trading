5db9994dd9899c4a6389a04278a0543d325de981
Tyoukai <zhang_k_w@163.com>
2024-11-19 14:43:32 +0800

    delete

--- python-learning/mplfinance_1/mplfinance_demo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import mplfinance as mpf
-import cufflinks as cf
-
-df = cf.datagen.ohlc()
-df['volume'] = np.random.randint(low=1000, high=10000, size=len(df))
-
-mpf.plot(df, type='candle', mav=(2, 5, 10), volume=True)
